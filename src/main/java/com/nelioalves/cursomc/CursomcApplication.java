b478a60f6ca661e92721846c3de5db49936e8503
William Keylon <williamgoncalves@msn.com>
2018-10-22 17:31:47 -0300

    REST Testado

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.algaworks.vendas;
+package com.nelioalves.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
